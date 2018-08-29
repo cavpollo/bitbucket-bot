56182b4753253677861853f185bc57434ef130d7
cavpollo <cvillagran@osigu.com>
2018-08-29 10:55:32 -0600

    log info

--- src/App.js
+++ src/App.js
@@ -176,6 +176,9 @@ function getRepositories(bitbucketOrganization, accessToken, page = 1) {
 
     return requestPromise(options)
         .then(async function (jsonResponse) {
+            console.log('jsonResponse')
+            console.log(jsonResponse)
+
             const repositories = []
             for (var i = 0, repository; repository = jsonResponse.values[i]; i++) {
                 // Get the repositories that are marked as services?
@@ -188,10 +191,16 @@ function getRepositories(bitbucketOrganization, accessToken, page = 1) {
                 repositories.push(repoData)
             }
 
+            console.log('repositories')
+            console.log(repositories)
+
             if (jsonResponse.next) {
                 const moreRepositories = await getRepositories(bitbucketOrganization, accessToken, page + 1)
                 repositories.concat(moreRepositories)
             }
+
+            console.log('repositories concat')
+            console.log(repositories)
 
             return repositories
         })
